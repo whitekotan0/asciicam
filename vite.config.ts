7cf7b5ecb86f655cb34b02e39edc6a0d8cf5fbd6
whitekotan0 <gcat5770@gmail.com>
2026-02-01 08:29:29 +0200

    feat: added deploy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/asciicam/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./', import.meta.url))
